679d1de91a5d92915fff1f504c328641ddd6d73d
paras <parasmanikc@gmail.com>
2020-08-03 21:59:27 +0545

    v2 update

--- routes/nav.js
+++ routes/nav.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send({ msg: 'Hello You Guys' });
+  res.send({ msg: 'Hello You Guys Haru pani' });
 });
 module.exports = router;
