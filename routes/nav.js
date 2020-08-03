5169d9881aed7d1d5fe658e853d219d8b50f6149
paras <parasmanikc@gmail.com>
2020-08-03 22:11:30 +0545

    okla

--- routes/nav.js
+++ routes/nav.js
@@ -4,4 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send({ msg: 'Hello You Guys in 2nd hai guys asdsa' });
 });
+
+router.get('/ola', (req, res) => {
+  res.send({ msg: 'Hello You Guys in 2nd hai guys asdsa' });
+});
 module.exports = router;
